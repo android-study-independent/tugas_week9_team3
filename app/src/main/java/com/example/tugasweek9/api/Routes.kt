22b6e29f8dc7385e8a8d97ef7ed3641fe815ffe7
Adi <haryadiwibowo87@gmail.com>
2023-10-15 15:27:37 +0700

    mengerjakan bagian Upcoming dan Details

--- app/src/main/java/com/example/tugasweek9/api/Routes.kt
+++ app/src/main/java/com/example/tugasweek9/api/Routes.kt
@@ -1,4 +1,17 @@
 package com.example.tugasweek9.api
 
+import com.example.tugasweek9.response.UpcomingResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface Routes {
+
+    @GET("movie/upcoming")
+    suspend fun getUpcoming(
+        @Query("language") lang: String = "en-EN",
+        @Query("page") page: Int
+    ): UpcomingResponse
+
+
+
 }
