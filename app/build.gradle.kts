ca2b55158f8a31a78c1a5a24c36a5085c3128b98
Nabila <134200060@student.upnyk.ac.id>
2023-10-21 21:42:06 +0700

    menambahkan implementation dalam gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation (platform("com.google.firebase:firebase-bom:32.4.0"))
+    implementation ("com.google.firebase:firebase-analytics-ktx")
 
 
 
